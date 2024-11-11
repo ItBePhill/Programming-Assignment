5f438fdeb523d03d9615127a7f209d697efbe96b
Phillip Wood <PWood9@uclan.ac.uk>
2024-11-11 14:45:15 +0000

    Start

--- Programming Assignment/Source.cpp
+++ Programming Assignment/Source.cpp
@@ -1,7 +1,33 @@
 // Programming Assignment 
 #include <iostream>
+#include <map>
 using namespace std;
 
-int main() {
 
+//Classes
+class Potato {
+public:
+	string name;
+	double price;
+};
+
+class Topping {
+public:
+	string name;
+	double price;
+};
+
+class Extra {
+public:
+	string name;
+	double price;
+};
+
+
+int main() {
+	Extra extra;
+	extra.name = "Salad Box";
+	extra.price = 1.50;
+	cout << "Welcome to Hot Potato!\nWhat woudl you like to do?\n1 - Add Credits\n2 - Create an Order\n3 - View Recent Orders";
+	cout << endl << extra.name << " " << extra.price;
 }
