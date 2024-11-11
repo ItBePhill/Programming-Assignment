13923c98af0ea4ae82dc79f6404d1fd6cd179c76
phill <phillipwood01@gmail.com>
2024-11-11 15:12:06 +0000

    Added TODO List and added functions for adding credits, creating orders and viewing recent orders

--- Programming Assignment/Source.cpp
+++ Programming Assignment/Source.cpp
@@ -4,30 +4,76 @@
 using namespace std;
 
 
-//Classes
-class Potato {
-public:
-	string name;
-	double price;
-};
 
-class Topping {
-public:
-	string name;
-	double price;
-};
+/*
+TODO:
+- Research how to read and write to files, probably JSON or maybe an sqlite db for the potato / toppings / extras.
+- Finish Add Credits Function
+- Finish Create Order Function
+- Finish View Recent Function
+- 
+*/
 
-class Extra {
-public:
-	string name;
-	double price;
-};
 
+//Classes Not Done / Don't know if gonna use
+//class Potato {
+//public:
+//	string name;
+//	double price;
+//};
+//
+//class Topping {
+//public:
+//	string name;
+//	double price;
+//};
+//
+//class Extra {
+//public:
+//	string name;
+//	double price;
+//};
+
+void addCredits() {
+	int creditAnswer;
+	cout << endl << "-----Add Credits-----\nCurrent Credits: ";
+	cout << endl << "";
+}
+void createOrder() {
+	int orderAnswer;
+	cout << endl << "----Create Order-----";
+}
+void viewRecent() {
+	cout << endl << "----View Recent-----";
+}
 
 int main() {
-	Extra extra;
-	extra.name = "Salad Box";
-	extra.price = 1.50;
-	cout << "Welcome to Hot Potato!\nWhat woudl you like to do?\n1 - Add Credits\n2 - Create an Order\n3 - View Recent Orders";
-	cout << endl << extra.name << " " << extra.price;
+	int welcomeAnswer;
+	bool breakwhile = false;
+	//loop forever until user types answer
+	while (!breakwhile) {
+		cout << "Welcome to Hot Potato!\nWhat would you like to do?\n1 - Add Credits\n2 - Create an Order\n3 - View Recent Orders\n- ";
+		cin >> welcomeAnswer;
+
+		//check if answer is valid
+		switch (welcomeAnswer) {
+			case 1:
+				addCredits();
+				breakwhile = true;
+				break;
+			case 2:
+				createOrder();
+				breakwhile = true;
+				break;
+			case 3:
+				viewRecent();
+				breakwhile = true;
+				break;
+			default:
+				cout << "\nError: Invalid Option\n";
+				break;
+		}
+	}
+
 }
+
