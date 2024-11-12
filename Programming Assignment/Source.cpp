d68a2b0c7accf5f4c0860d3789600a9eed1f61a9
Phillip Wood <PWood9@uclan.ac.uk>
2024-11-12 10:04:02 +0000

    worked on add credits

--- Programming Assignment/Source.cpp
+++ Programming Assignment/Source.cpp
@@ -1,18 +1,23 @@
 // Programming Assignment
+// Each reference will be at bottom of file and will be numbered
 //Brief: https://portal.uclan.ac.uk/ultra/courses/_177144_1/outline/file/_7529994_1
+
+
 #include <iostream>
-#include <map>
+#include <functional>
+#include <time.h>
 using namespace std;
 
-
-
 /*
-TODO:
-- Research how to read and write to files, probably JSON or maybe an sqlite db for the potato / toppings / extras
+TODO:		key: 
+			 // done
+			 / doing
+- Research how to read and write to files, probably JSON or maybe an sqlite db
 - Research Constant Variables
-- Finish Add Credits Function
+- Finish Add Credits Function /
 - Finish Create Order Function
 - Finish View Recent Function
+- Add Sleep to Quit Function
 */
 
 
@@ -35,17 +40,45 @@ TODO:
 //	double price;
 //};
 
-void addCredits() {
-	int creditAnswer;
-	cout << endl << "-----Add Credits-----\nCurrent Credits: ";
-	cout << endl << "";
+//reference 1 
+void addCredits(std::function<void()> ptr) {
+	// Set Variables
+	double creditAnswer;
+	string sure = "n";
+	double credits = 0;
+
+	cout << endl << "-----Add Credits-----\nCurrent Credits: " << credits;
+	//ask user to input an amount of credits then ask if they're sure and check
+	while (sure != "y") {
+		while (true) {
+			cout << endl << "How many credits would you like to add?\n- ";
+			cin >> creditAnswer;
+			cout << endl << "Are you Sure? (y/n)";
+			cin >> sure;
+			if (sure == "y") {
+				break;
+			}
+			else if (sure == "n") {
+				sure = "n";
+				continue;
+			}
+			else {
+				cout << "Error: Invalid Option";
+			}
+			
+		}
+		credits += creditAnswer;
+		int ans;
+		cout << endl << "New Amount: " << credits << endl << endl;
+		ptr();
+	}
+
 }
-void createOrder() {
-	int orderAnswer;
-	cout << endl << "----Create Order-----";
+void createOrder(std::function<void()> ptr) {
+	cout << endl << "----Create A New Order-----";
 }
-void viewRecent() {
-	cout << endl << "----View Recent-----";
+void viewRecent(std::function<void()> ptr) {
+	cout << endl << "----View Recent Orders-----";
 }
 
 int main() {
@@ -53,23 +86,32 @@ int main() {
 	bool breakwhile = false;
 	//loop forever until user types answer
 	while (!breakwhile) {
-		cout << "Welcome to Hot Potato!\nWhat would you like to do?\n1 - Add Credits\n2 - Create an Order\n3 - View Recent Orders\n- ";
+		cout << "----Welcome to Hot Potato!----\nWhat would you like to do?\n1 - Add Credits\n2 - Create an Order\n3 - View Recent Orders\n4 - Quit";
 		cin >> welcomeAnswer;
 
 		//check if answer is valid
 		switch (welcomeAnswer) {
 			case 1:
-				addCredits();
+				addCredits(main);
 				breakwhile = true;
 				break;
 			case 2:
-				createOrder();
+				createOrder(main);
 				breakwhile = true;
 				break;
 			case 3:
-				viewRecent();
+				viewRecent(main);
 				breakwhile = true;
 				break;
+			
+			case 4:
+				cout << "Bye!";
+				cout << "Quitting in...";
+				for (int i = 0; i < 2; i++) {
+					cout << i+1;
+				}
+				quick_exit(0);
+	
 			default:
 				cout << "\nError: Invalid Option\n";
 				break;
@@ -78,3 +120,16 @@ int main() {
 
 }
 
+
+
+
+
+
+
+/*
+References:
+
+1 - https://www.quora.com/How-do-you-pass-a-function-to-another-function-in-C-and-what-is-its-use
+
+
+*/
